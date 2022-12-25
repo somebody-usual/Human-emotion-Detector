8c2127a71ebc868e471929730774c607209e124b
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2022-12-25 15:47:21 +0530

    Add files via upload

---
+++ train 1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+from keras.emotion_models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+train_dir = 'data/train1'
+val_dir = 'data/test'
